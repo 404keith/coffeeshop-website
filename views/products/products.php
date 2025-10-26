d1978cd2e60d700e21a87a1cefd3099dcdb0b9c5
404keith <keithantonio444@gmail.com>
2025-10-26 20:45:28 +0800

    fixed product mobile view

--- views/products/products.php
+++ views/products/products.php
@@ -66,7 +66,7 @@ switch ($currentURI) {
       <div class="row g-4 justify-content-center">
         <?php foreach ($products as $product):
           $is_in_stock = $product['stock'] > 0;
-          $stock_text = $is_in_stock ? 'In Stock' : 'Out of Stock';
+          $stock_text = $is_in_stock ? 'Available' : 'Not Available';
           $stock_color = $is_in_stock ? 'text-success' : 'text-danger';
           ?>
           <div class="col-12 col-sm-6 col-md-4 d-flex">
@@ -76,14 +76,18 @@ switch ($currentURI) {
 
               <div class="card-body d-flex flex-column">
                 <div class="d-flex justify-content-between align-items-center card-text">
+                  <!--  title -->
                   <h5 class="card-title mb-0 fw-bold"><?= htmlspecialchars($product['name']) ?></h5>
+                  <!--  price  -->
                   <p class="fw-bold mb-0 price">P <?= number_format($product['price'], 2) ?></p>
                 </div>
+                <!--  description  -->
                 <p class="fs-6 flex-grow-1"><?= htmlspecialchars($product['description']) ?></p>
 
                 <!-- items inside cart: bottom part -->
-                <p class="mb-2 fw-semibold <?= $stock_color ?>">
-                  <?= $stock_text ?> (<?= $product['stock'] ?> available)
+                <!-- stock status -->
+                <p class="stock mb-2 fw-semibold <?= $stock_color ?>">
+                  <?= $stock_text ?>
                 </p>
 
                 <form method="POST" action="<?= FILE_ROOT ?>/cart-actions">
@@ -97,9 +101,11 @@ switch ($currentURI) {
                     <i class="bi bi-star-fill"></i>
                     <i class="bi bi-star-fill"></i>
                   </div>
+                  <!-- product type -->
                   <h5 class="card-title product_type">
                     <?= strtoupper(htmlspecialchars($product['product_type'])) ?>
                   </h5>
+                  <!-- add to cart btn -->
                   <div class="d-flex gap-2">
                     <button type="submit" class="btn btn-primary btn-rounded w-75" <?= $is_in_stock ? '' : 'disabled' ?>>
                       <?= $is_in_stock ? 'Add to Cart' : 'Out of Stock' ?>
