83979ca482928dd527655f6b64d72ba4215c7284
404keith <keithantonio444@gmail.com>
2025-10-29 05:25:55 +0800

    fixed contact us mpbile view

--- views/home/contact.php
+++ views/home/contact.php
@@ -10,97 +10,7 @@ include APP_ROOT . '/views/layouts/header.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <style>
-
-        body {
-  background-image: url("../images/login-background.png");
-  background-color: #fae5cc;
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.container-fluid.d-flex.justify-content-center {
-  flex-grow: 1;
-}
-
-.form-width {
-  max-width: 600px;
-  width: 100%;
-  height: auto;
-  border-radius: 3rem;
-  background-color: #fff6eb;
-  margin: 3rem auto 5rem;
-}
-
-.form-outline .form-control:focus ~ .form-label {
-  color: #d48423;
-}
-
-.form-outline .form-control:focus {
-  background-color: #fff6eb;
-  border: 1px solid #d48423;
-}
-
-.form-check-input:checked {
-  background-color: #d48423 !important;
-  border-color: #d48423 !important;
-}
-
-.btn-primary {
-  background-color: #d48423 !important;
-  border-color: #d48423 !important;
-}
-
-.btn-primary:hover {
-  background-color: #c3680b !important;
-  border-color: #c3680b !important;
-}
-
-.text-center a {
-  color: #d48423;
-  text-decoration: none;
-}
-
-.text-center a:hover {
-  color: #c3680b;
-  text-decoration: underline;
-}
-
-.form-padding {
-  padding-top: 6rem;
-  padding-left: 6rem;
-  padding-right: 6rem;
-}
-
-.alert {
-  text-align: center;
-}
-
-.welcome-text {
-  color: #d48423;
-  text-align: center;
-  font-size: 5rem;
-  font-family: campana;
-  margin: -4rem 1.3rem 0 0;
-  padding-bottom: 1rem;
-}
-
-.login-text {
-  color: #c3680b;
-  font-size: 15px;
-  font-family: inter;
-  font-weight: bolder;
-  text-align: center;
-  margin-top: -1.5rem;
-  margin-bottom: 1.5rem;
-}
-
-    </style>
+    <link rel="stylesheet" href="<?= FILE_ROOT ?>/public/assets/css/contact.css" />
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -148,8 +58,9 @@ include APP_ROOT . '/views/layouts/header.php';
                     <button type="submit" class="btn btn-primary btn-block mb-4">Send Message</button>
 
                     <!-- Extra Info -->
-                    <div class="text-center">
-                        <p>Or reach us at <a href="mailto:support@yourcompany.com">mondaymornings.test123@gmail.com</a></p>
+                    <div class="text-center bottom-text">
+                        <p>Or reach us at <a href="mailto:support@yourcompany.com">mondaymornings.test123@gmail.com</a>
+                        </p>
                     </div>
 
                 </div>
@@ -158,7 +69,7 @@ include APP_ROOT . '/views/layouts/header.php';
         </div>
     </main>
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             // Initialize MDBootstrap form components
             document.querySelectorAll('.form-outline').forEach((formOutline) => {
                 new mdb.Input(formOutline).init();
